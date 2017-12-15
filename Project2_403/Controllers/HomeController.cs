c0ba307ccd620c928918d651b4184d23fbe01a9b
gabrielmc94 <30884035+gabrielmc94@users.noreply.github.com>
2017-12-14 23:55:47 -0700

    added more commentation stuffsss

--- Project2_403/Controllers/HomeController.cs
+++ Project2_403/Controllers/HomeController.cs
@@ -5,6 +5,18 @@ using System.Web;
 using System.Web.Mvc;
 using Project2_403.Models;
 
+/*
+    This project is designed to simplify the device repair process for Tricked Out Accessories, a phone accessories and repair company.
+    Their current system only uses paper documents and is very unorganized.  This application should allow employees to enter repair forms with
+    information about the devices they are repairing and the customers who own those devices.  It should also allow managers to see all repairs
+    in progress and allow them to update their knowledge base (Or Tech Specs) of phones.
+
+    All users are required to login before viewing any of this data - either through individual logins or through OAuth - because this is not a
+    client-facing application.  It is solely for the use of company employees
+
+*/
+
+
 namespace Project2_403.Controllers
 {
     [Authorize]
